713069956aa6f076012fad6aeec0eb3dd53c8bed
Alex-Astolfi <alex.astolfi00@gmail.com>
2024-02-14 13:21:21 +0100

    Compute Surprsal

--- experiments/Surprisal/compute_surprisal.py
+++ experiments/Surprisal/compute_surprisal.py
@@ -1,23 +1,22 @@
 from surprisal_utils import *
 from transformers import RobertaForMaskedLM, RobertaTokenizer
 import numpy as np
+import json
 
 model_name = 'roberta-base'
 model = RobertaForMaskedLM.from_pretrained(model_name)
 tokenizer = RobertaTokenizer.from_pretrained(model_name)
 
-threshold_path="../../datasets/threshold.json"
+threshold_path="./datasets/threshold.json"
 dataset_path="./datasets/combined_aro_valse.json"
 
-threshold_foils=load_foils(threshold_path)
-threshold_surprisals=compute_surprisal(threshold_foils,tokenizer,model)
+with open(dataset_path, 'r') as f:
+    data = json.load(f)
 
-sents=["A woman bothers the usher.","An usher bothers the woman.","The food packages a man.","I eat pizza.","A pen eats a table."]
-surprisals=compute_surprisal(sents,tokenizer,model)
-print(surprisals)
-#min_surprisal=(min(threshold_surprisals))
-#max_surprisal=(max(threshold_surprisals))
-#max_index=np.argmax(threshold_surprisals)
-#print(min_surprisal)
-#print(max_surprisal)
-#print(max_index)
+for sample in data.values():
+    foil=sample['foil_active']
+    surprisal=sentence_surprisal(foil,tokenizer,model)
+    sample['surprisal']=surprisal
+
+with open(dataset_path, 'w') as f:
+    json.dump(data, f, indent=4)
--- experiments/Surprisal/surprisal_utils.py
+++ experiments/Surprisal/surprisal_utils.py
@@ -17,29 +17,31 @@ def load_foils(json_path):
     return foils
 
 def compute_surprisal(sentences,tokenizer,model):
-    model.eval()
     # Initialize the list to store the surprisal values
     surprisal_scores = []
     # Iterate through the dataset
     for sent in sentences:
-        word_surprisals=[]
-        # Tokenize the input
-        input_ids = torch.tensor(tokenizer.encode(sent,add_special_tokens=True)).unsqueeze(0)
-        mask_indexes=torch.arange(1,len(input_ids[0])-1) #remove the beginning and end of sentence tokens
-        for index in mask_indexes:
-            token_id=input_ids[0][index]
-
-            if (tokenizer.convert_ids_to_tokens(token_id.item()).replace("Ġ","").lower() not in stopwords.words('english')):
-                #print(tokenizer.convert_ids_to_tokens(token_id.item()).replace("Ġ","").lower())
-                masked_input_ids=input_ids.clone()
-                # Forward pass to compute the layer outputs
-                masked_input_ids[0][index] = tokenizer.convert_tokens_to_ids(['[MASK]'])[0]
-                with torch.no_grad():
-                    outputs = model(masked_input_ids)
-                    logits = outputs.logits
-                # Compute the surprisal
-                surprisal = token_surprisal(logits, token_id, index)
-                word_surprisals.append(surprisal)
-        surprisal_scores.append(sum(word_surprisals)/len(word_surprisals))
+        surprisal=sentence_surprisal(sent,tokenizer,model)
+        surprisal_scores.append(surprisal)
 
     return surprisal_scores
+
+def sentence_surprisal(sent,tokenizer,model):
+    model.eval()
+    word_surprisals=[]
+    # Tokenize the input
+    input_ids = torch.tensor(tokenizer.encode(sent,add_special_tokens=True)).unsqueeze(0)
+    mask_indexes=torch.arange(1,len(input_ids[0])-1) #remove the beginning and end of sentence tokens
+    for index in mask_indexes:
+        token_id=input_ids[0][index]
+        if (tokenizer.convert_ids_to_tokens(token_id.item()).replace("Ġ","").lower() not in stopwords.words('english')):                #print(tokenizer.convert_ids_to_tokens(token_id.item()).replace("Ġ","").lower())
+            masked_input_ids=input_ids.clone()
+            # Forward pass to compute the layer outputs
+            masked_input_ids[0][index] = tokenizer.convert_tokens_to_ids(['[MASK]'])[0]
+            with torch.no_grad():
+                outputs = model(masked_input_ids)
+                logits = outputs.logits
+            # Compute the surprisal
+            surprisal = token_surprisal(logits, token_id, index)
+            word_surprisals.append(surprisal)
+    return sum(word_surprisals)/len(word_surprisals)
