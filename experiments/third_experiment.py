06ead22adc39443458eac2adc4ef692711e8ba76
momentino <filippomomente@protonmail.com>
2024-02-16 22:45:24 +0100

    X2VLM pre

--- experiments/third_experiment.py
+++ experiments/third_experiment.py
@@ -37,7 +37,7 @@ _logger.info(f"Running with args {FLAGS}, {FIRE_FLAGS}")
 """
 def get_args_parser():
     parser = argparse.ArgumentParser('Set parameters for the expriments', add_help=False)
-    parser.add_argument('--model', default='NegCLIP', type=str, choices=['ALBEF','XVLM','BLIP','X2VLM','NegCLIP'])
+    parser.add_argument('--model', default='BLIP', type=str, choices=['ALBEF','XVLM','BLIP','X2VLM','NegCLIP'])
     parser.add_argument('--experiment', default='third', type=str, choices=['third'])
     parser.add_argument('--dataset', default='all', type=str, choices=['VALSE', 'ARO','all'])
 
